144d7483453ea2c2e1fa02499b57acd3b48e1cf8
Eugine Onyango <eugineogutu004@gmail.com>
2025-04-23 13:35:39 +0300

    Added files

---
+++ userdefinedfunctions.py
@@ -0,0 +1,18 @@
+#python stardard library functions
+#1.import()
+#2.abs()
+#3.round()
+#4.print()
+#5.input()
+#6.print()
+#7.len()
+def hello():
+ print('Howdy!')
+ print('Howdy!!!')
+ print('Hello there.')
+hello()
+#user defined functions 
+#A major purpose of functions is to group code that gets executed multiple times. Without a function defined, you would have to copy and paste 
+#.....this code each time
+
+
